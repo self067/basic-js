5090508921be16165c171b15860c78abafcd6753
self067 <seltor@mail.ru>
2021-01-21 15:35:18 +0300

    feat: add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,8 +2,14 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const LN2 = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sa) {
+  if(!sa || typeof sa !== 'string' )  return false;
+  const psa = parseFloat(sa);
+ 
+  if( isNaN(psa) || psa<=0 || psa > 15) return false;
+  
+  return  Math.ceil(Math.log2(MODERN_ACTIVITY/psa) / (LN2 / HALF_LIFE_PERIOD));
+
 };
