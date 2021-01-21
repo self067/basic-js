df15b1fc23dafadddbf7e7ef5e9595b0d52cc5e7
self067 <seltor@mail.ru>
2021-01-21 16:03:54 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if( !Array.isArray( members)) return false;
+  if( members.length === 0 ) return '';
+  return members.filter((v)=> v && typeof v === 'string')
+                .map((v)=> v.trim().toUpperCase()[0])
+                .sort()
+                .reduce((p,c)=>p+c);
 };
