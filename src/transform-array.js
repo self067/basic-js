ff0a70115c40ef7d06ca904e1d6341e48b5ebbb9
self067 <seltor@mail.ru>
2021-01-24 19:44:05 +0300

    feat: add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+  
+  if(!(arr instanceof Array)) throw('Error');
+  let prev=null;
+  const re = [];
+
+ for(let a=0,r=0; a < arr.length; a++) {
+  switch( arr[a]){
+    case '--discard-next': prev = null; if( a < arr.length-1 ) a++; break;
+    case '--discard-prev': if( re.length > 0 && prev!== null) re.pop(); break;
+    case '--double-next':  if( a < arr.length-1 ) re.push( arr[a+1] ); break;
+    case '--double-prev': if( re.length > 0 && prev!== null) re.push( prev ); break;
+    default: prev = arr[a]; re.push(prev); break;
+  }
+ }
+  return re;
 };
