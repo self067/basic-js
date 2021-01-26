8f4d69a7d17efde35fc601a3993bdc0ca01053ef
self067 <seltor@mail.ru>
2021-01-26 18:04:05 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+  console.log(str, options);
+  let  res = '';
+  let addstr = '';
+
+  if( options.additionRepeatTimes) { 
+    addstr = addition;
+    for( let i = 0; i < options.additionRepeatTimes; i++) 
+      addstr += options.additionSeparator + addstr;
+  }
+
+
+  if( options.repeatTimes) { 
+    res = str;
+    for( let i = 1; i < options.repeatTimes; i++) 
+      res += addstr + options.separator + str;
+  }
+  return res;
+  // for( let i = )
+/*
+	`repeatTimes` устанавливает число повторений `str`
+  - `separator` это строка, разделяющая повторения `str`
+  - `addition` это дополнительная строка, которая будет добавлена после каждого повторения `str`
+  - `additionRepeatTimes` устанавливает число повторений `addition`
+  - `additionSeparator` это строка, разделяющая повторения `addition`
+*/
 };
   
