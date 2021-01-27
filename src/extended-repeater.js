1039e4039f0165036e4ae80419088436fd2feb47
self067 <seltor@mail.ru>
2021-01-27 18:04:02 +0300

    fix: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,24 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater( str, options ) {
-  console.log(str, options);
   let  res = '';
   let addstr = '';
 
-  if( options.additionRepeatTimes) { 
+  const separator = (options.separator)?options.separator:'+';
+  const repeatTimes = (options.repeatTimes)?options.repeatTimes:0;
+
+  const addition = options.addition; //)?options.addition:'';
+  const additionSeparator = (options.additionSeparator)?options.additionSeparator:'|';
+  const additionRepeatTimes = (options.additionRepeatTimes)?options.additionRepeatTimes:0;
+
+  console.log(str, options);
+
+
+  if( addition && additionRepeatTimes) { 
     addstr = addition;
-    for( let i = 0; i < options.additionRepeatTimes; i++) 
-      addstr += options.additionSeparator + addstr;
+    for( let i = 1; i < additionRepeatTimes; i++) 
+      addstr += additionSeparator; // + addstr;
   }
+console.log('addstr= ', addstr, ' ===');
 
-
-  if( options.repeatTimes) { 
+  if( repeatTimes) { 
     res = str;
-    for( let i = 1; i < options.repeatTimes; i++) 
-      res += addstr + options.separator + str;
+    for( let i = 1; i < repeatTimes; i++) 
+      res += addstr + separator + str;
   }
+  res += addstr;
+  console.log(res);
+
   return res;
-  // for( let i = )
 /*
 	`repeatTimes` устанавливает число повторений `str`
   - `separator` это строка, разделяющая повторения `str`
