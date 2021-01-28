68332f788da7a3a7a6a699d279a0f1a619ee3e81
self067 <seltor@mail.ru>
2021-01-28 17:24:46 +0300

    fix: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -5,37 +5,30 @@ module.exports = function repeater( str, options ) {
   let addstr = '';
 
   const separator = (options.separator)?options.separator:'+';
-  const repeatTimes = (options.repeatTimes)?options.repeatTimes:0;
+  const repeatTimes = (options.repeatTimes)?options.repeatTimes:("repeatTimes" in options)?1:0;
 
-  const addition = options.addition; //)?options.addition:'';
+  const addition = ''+options.addition; //)?options.addition:'';
   const additionSeparator = (options.additionSeparator)?options.additionSeparator:'|';
-  const additionRepeatTimes = (options.additionRepeatTimes)?options.additionRepeatTimes:0;
+  const additionRepeatTimes = (options.additionRepeatTimes)?options.additionRepeatTimes: ("additionRepeatTimes" in options)?1:0;
 
-  console.log(str, options);
-
+  // console.log(str, options, repeatTimes, additionRepeatTimes);
 
   if( addition && additionRepeatTimes) { 
     addstr = addition;
     for( let i = 1; i < additionRepeatTimes; i++) 
-      addstr += additionSeparator; // + addstr;
+      addstr += additionSeparator + addition; // + addstr;
   }
-console.log('addstr= ', addstr, ' ===');
+// console.log('addstr= ', addstr, ' ===');
 
   if( repeatTimes) { 
-    res = str;
+    res = ''+str;
     for( let i = 1; i < repeatTimes; i++) 
       res += addstr + separator + str;
   }
   res += addstr;
-  console.log(res);
+  // console.log(res);
 
   return res;
-/*
-	`repeatTimes` устанавливает число повторений `str`
-  - `separator` это строка, разделяющая повторения `str`
-  - `addition` это дополнительная строка, которая будет добавлена после каждого повторения `str`
-  - `additionRepeatTimes` устанавливает число повторений `addition`
-  - `additionSeparator` это строка, разделяющая повторения `addition`
-*/
+
 };
   
