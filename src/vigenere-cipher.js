aa12780e920ee3e1546a53ad983cabcac612e950
self067 <seltor@mail.ru>
2021-01-30 21:04:00 +0300

    fix: vigenere-cipher add decode

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -2,18 +2,43 @@ const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
   constructor(dir) {
-    this.dir = dir;
+    if( dir=== undefined) this.dir = true;
+    else this.dir = dir;
   }
-  encrypt( message, key) {
+  encrypt( message, keys) {
+    const key = keys.toUpperCase();
+    const keylen = key.length;
+    let ki = 0;
 
-    const m = ''+message.toUpperCase.split('').map((e)=>e);
-//- 65 A
-
+    let res = (''+message).toUpperCase().split('').map((m)=>{
+      if(m.charCodeAt(0)<65 || m.charCodeAt(0)>90 )return m;
+      const k = key.charCodeAt(ki%keylen);
+      const en = String.fromCharCode(( m.charCodeAt(0) + k ) % 26 +65);
+      ki++;
+      return en;
+    });
     // c =  ( m + k ) % 26;
     // m =  ( c - k + 26) mod 26;
+   
+    if(!this.dir) res = res.reverse();
+    return res.join('');
   }    
-  decrypt( message, key) {
+  decrypt( message, keys) {
+    const key = keys.toUpperCase();
+    const keylen = key.length;
+    let ki = 0;
 
+    let res = (''+message).toUpperCase().split('').map((m)=>{
+      if(m.charCodeAt(0)<65 || m.charCodeAt(0)>90 )return m;
+      const k = key.charCodeAt(ki%keylen);
+      const en = String.fromCharCode(( m.charCodeAt(0) - k +26) % 26 +65);
+      ki++;
+      return en;
+    });
+    // c =  ( m + k ) % 26;
+    // m =  ( c - k + 26) mod 26;
+    if(!this.dir) res = res.reverse();
+    return res.join('');
   }
 }
 
